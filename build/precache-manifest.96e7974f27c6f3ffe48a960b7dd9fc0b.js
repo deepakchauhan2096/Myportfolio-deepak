7bc76c7bfc1cd0840fc90f208b3c003202f0538e
deepakchauhan2096 <deepakchauhan2096@gmail.com>
2022-08-08 15:23:00 +0530

    dd

---
+++ build/precache-manifest.96e7974f27c6f3ffe48a960b7dd9fc0b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd26a8e39cb0e0e609fdb0e4caa35f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b020790b3620bb7125",
+    "url": "/static/css/main.b1204bcd.chunk.css"
+  },
+  {
+    "revision": "8b6d93a7b3a291305236",
+    "url": "/static/js/2.eb768859.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.eb768859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b020790b3620bb7125",
+    "url": "/static/js/main.c3f92f4d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b0ff01843b36784a165549788b71f65a",
+    "url": "/static/media/avatar.b0ff0184.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  }
+]);
